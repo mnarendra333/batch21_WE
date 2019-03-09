0cf6e78b1e4edc0d0d0a54a03c82ffaf7d078574
Narendra <mnarendra333@gmail.com>
2019-03-09 11:49:51 +0530

    hibfirstapp

---
+++ Hib_ws/HibFirstApp/src/com/pragim/model/Product.java
@@ -0,0 +1,39 @@
+package com.pragim.model;
+
+public class Product {
+
+	@Override
+	public String toString() {
+		return "Product [proId=" + proId + ", proName=" + proName + ", price="
+				+ price + "]";
+	}
+
+	private int proId;
+	private String proName;
+	private double price;
+
+	public int getProId() {
+		return proId;
+	}
+
+	public void setProId(int proId) {
+		this.proId = proId;
+	}
+
+	public String getProName() {
+		return proName;
+	}
+
+	public void setProName(String proName) {
+		this.proName = proName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
